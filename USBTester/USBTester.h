d0eb99b025828aff3acd08c3f47f222d5de57705
Mike Hagedorn <mike@silverchairsolutions.com>
2015-09-18 15:33:13 -0400

    Initial Commit

---
+++ USBTester/USBTester.h
@@ -0,0 +1,13 @@
+//
+//  USBTester.h
+//  USBTester
+//
+//  Created by Mike Hagedorn on 9/18/15.
+//  Copyright (c) 2015 Mike Hagedorn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface USBTester : NSObject
+
+@end
---
+++ USBTester/USBTester.m
@@ -0,0 +1,13 @@
+//
+//  USBTester.m
+//  USBTester
+//
+//  Created by Mike Hagedorn on 9/18/15.
+//  Copyright (c) 2015 Mike Hagedorn. All rights reserved.
+//
+
+#import "USBTester.h"
+
+@implementation USBTester
+
+@end
